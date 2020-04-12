edcb1b86dc1d47d9fdbe3f63b2e7b2bbf594f9fb
Maddison Hellstrom <maddy@na.ai>
2020-04-12 08:02:27 -0700

    [source/sniputils:tweak] improve sniputils
    general:
    - lint/format code with bandit, prospector, vulture
    - accept snip obj as arg and update it automatically
    
    complete/completion:
    - handle case where no choices match
    - make separator configurable

--- pythonx/sniputils.py
+++ pythonx/sniputils.py
@@ -1,10 +1,18 @@
-def complete(t, choices):
-	if t:
-		t = t.split(",")[-1]
-		choices = [ m[len(t):] for m in choices if m.startswith(t) ]
-	if len(choices) == 1:
-		return choices[0]
-	return "(" + "|".join(choices) + ")"
+def complete(snip, t, choices, sep):
+  if len(t) > 0:
+    # allow selection of multiple choices separated by ','
+    t = t.split(sep)[-1]
+    # narrow choices matching prefix string
+    choices = [m[len(t):] for m in choices if m.startswith(t)]
+  if len(choices) == 0:
+    snip.rv = ""
+  # if narrowed down to one choice, return it plainly, prefixed with prefix
+  elif len(choices) == 1:
+    snip.rv = choices[0]
+  else:
+    # wrap choices in parens, separate by pipes
+    snip.rv = "(" + "|".join(choices) + ")"
 
-def completion(choices):
-	return lambda t: complete(t, choices)
+
+def completion(choices, sep=","):
+  return lambda snip, t: complete(snip, t, choices, sep)
