ec6f506732d6915e87575cf3188e9de0f3945ce9
Maddison Hellstrom <maddy@na.ai>
2022-03-28 03:50:44 -0700

    Cleanup

--- lua/user/plugin/vcoolor.lua
+++ lua/user/plugin/vcoolor.lua
@@ -1,7 +1,7 @@
-vim.notify 'vcoolor'
 ---- KabbAmine/vCoolor.vim
 vim.g.vcoolor_lowercase = 0
 vim.g.vcoolor_disable_mappings = 1
+
 -- Use yad as the color picker (Linux)
 if vim.fn.has 'unix' then
   vim.g.vcoolor_custom_picker = table.concat({
