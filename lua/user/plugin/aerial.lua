74ee931b2c6d1a562122254cc9a64e71458a1f4c
Maddison Hellstrom <maddy@na.ai>
2022-04-05 21:19:11 -0700

    Update aerial config

--- lua/user/plugin/aerial.lua
+++ lua/user/plugin/aerial.lua
@@ -1,6 +1,8 @@
 ---- stevearc/aerial.nvim
 local fn = require 'user.fn'
 
+local update_delay = 500
+
 require('aerial').setup {
   backends = { 'lsp', 'treesitter', 'markdown' },
   close_behavior = 'global',
@@ -22,6 +24,17 @@ require('aerial').setup {
   min_width = 30,
   placement_editor_edge = true,
   update_events = 'TextChanged,InsertLeave',
+  lsp = {
+    update_when_errors = true,
+    diagnostics_trigger_update = true,
+    update_delay = update_delay,
+  },
+  treesitter = {
+    update_delay = update_delay,
+  },
+  markdown = {
+    update_delay = update_delay,
+  },
 }
 
 local treesitter_langs = require 'aerial.backends.treesitter.language_kind_map'
