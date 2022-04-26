9a6b9f9d096c7411e708d7361a6014b372cd4b07
Maddison Hellstrom <maddy@na.ai>
2022-04-25 19:45:43 -0700

    Call user.packer.install_or_sync if failed to load impatient.nvim

--- init.lua
+++ init.lua
@@ -1,11 +1,14 @@
 -- XXX: impatient.nvim will only be required until https://github.com/neovim/neovim/pull/15436 is merged
 if not pcall(require, 'impatient') then
+  print 'failed to load impatient.nvim'
   -- if unable to load impatient.nvim, try loading user.packer to see if
   -- packer needs to be installed
-  if not pcall(require, 'user.packer') then
+  local ok, user_packer = pcall(require, 'user.packer')
+  if not ok then
     print 'failed to load impatient.nvim and user.packer'
     return
   end
+  user_packer.install_or_sync()
 end
 
 require 'user.settings'
