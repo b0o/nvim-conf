ba39fcab91e1a6e0d71ec0f62514c17aa94020b9
Maddison Hellstrom <maddy@na.ai>
2023-04-15 18:26:09 -0700

    add actions for inserting and copying include for header

--- lua/user/plugin/easypick.lua
+++ lua/user/plugin/easypick.lua
@@ -1,4 +1,42 @@
 local easypick = require 'easypick'
+local actions = require 'telescope.actions'
+local action_state = require 'telescope.actions.state'
+local xk = require('user.mappings').xk
+
+local find_first_include = function(bufnr)
+  local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+  for i, line in ipairs(lines) do
+    if line:find '^#include' then
+      return i
+    end
+  end
+  return nil
+end
+
+local insert_action = function(current_line)
+  return function(prompt_bufnr)
+    local picker = action_state.get_current_picker(prompt_bufnr)
+    local orig_win_id = picker.original_win_id
+    local orig_bufnr = vim.api.nvim_win_get_buf(orig_win_id)
+
+    local entry = action_state.get_selected_entry()
+    local include = entry.include
+    if include == nil then
+      return
+    end
+
+    local target_line = not current_line and find_first_include(orig_bufnr) or nil
+    if target_line ~= nil then
+      target_line = target_line - 1
+    else
+      target_line = vim.api.nvim_win_get_cursor(orig_win_id)[1] - 1
+    end
+
+    actions.close(prompt_bufnr)
+
+    vim.api.nvim_buf_set_lines(orig_bufnr, target_line, target_line, false, { include })
+  end
+end
 
 easypick.setup {
   pickers = {
@@ -6,6 +44,22 @@ easypick.setup {
       name = 'headers',
       command = vim.fn.stdpath 'config' .. '/scripts/findheaders.py -f json',
       previewer = easypick.previewers.default(),
+      action = function(_, map)
+        map({ 'n', 'i' }, '<C-a>', insert_action(false))
+        map({ 'n', 'i' }, xk [[<C-S-a>]], insert_action(true))
+        map({ 'n', 'i' }, '<C-y>', function(prompt_bufnr)
+          local entry = action_state.get_selected_entry()
+          local include = entry.include
+          if include == nil then
+            return
+          end
+          actions.close(prompt_bufnr)
+          vim.fn.setreg('+', include)
+          vim.notify({ 'Copied', include }, vim.log.levels.INFO)
+        end)
+        return true
+      end,
+
       entry_maker = function(line)
         local entry = vim.fn.json_decode(line)
         if entry == nil or type(entry) ~= 'table' or entry.include_dir == nil or entry.header_file == nil then
@@ -21,6 +75,7 @@ easypick.setup {
           ordinal = full_path,
           display = entry.header_file,
           path = full_path,
+          include = '#include <' .. entry.header_file .. '>',
         }
       end,
     },
