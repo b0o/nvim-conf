7c634e3b905a3d5f821166161abe2b7bc54583a0
Maddison Hellstrom <maddy@na.ai>
2023-04-15 17:20:03 -0700

    remove duplicate easypick config

--- lua/user/plugin/telescope.lua
+++ lua/user/plugin/telescope.lua
@@ -9,35 +9,6 @@ local fn = require 'user.fn'
 local m = require 'user.mappings'
 local Debounce = require 'user.util.debounce'
 
-local easypick = require 'easypick'
-
-easypick.setup {
-  pickers = {
-    {
-      name = 'headers',
-      command = vim.fn.stdpath 'config' .. '/scripts/findheaders.py -f json',
-      previewer = easypick.previewers.default(),
-      entry_maker = function(line)
-        local entry = vim.fn.json_decode(line)
-        if entry == nil or type(entry) ~= 'table' or entry.include_dir == nil or entry.header_file == nil then
-          return {
-            value = line,
-            ordinal = line,
-            display = line,
-          }
-        end
-        local full_path = entry.include_dir .. '/' .. entry.header_file
-        return {
-          value = full_path,
-          ordinal = full_path,
-          display = entry.header_file,
-          path = full_path,
-        }
-      end,
-    },
-  },
-}
-
 local M = {}
 
 local dbounced_show_builtins = Debounce(function()
