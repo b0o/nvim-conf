06e3977a38a09904fa622dc99b6ed4696f0523f4
Maddison Hellstrom <maddy@na.ai>
2023-06-28 16:46:22 -0700

    add 'client' or 'server' to incline for react client/server files
    if the file has a 'use client' or 'use server' directive,
    add that to the incline statusline

--- lua/user/plugin/incline.lua
+++ lua/user/plugin/incline.lua
@@ -12,6 +12,53 @@ local extra_colors = {
   bg_nc = 'NONE',
 }
 
+local M = {}
+
+local react_use_directive_query_str = [[
+  (program
+    ((expression_statement (string (string_fragment) @directive))
+        (#any-of? @directive "use client" "use server")))
+]]
+
+-- lang -> query
+--- @type table<string, Query>
+local react_use_directive_query = {}
+
+-- bufnr -> {directive: string, timestamp: number}
+--- @type table<number, {directive: string, timestamp: number}>
+local get_react_use_directive_cache = {}
+
+--- @param bufnr number
+--- @return "use client" | "use server" | nil
+local function get_react_use_directive(bufnr)
+  local cached = get_react_use_directive_cache[bufnr]
+  if cached and cached.timestamp > vim.uv.now() - 1000 then
+    return cached.directive
+  end
+  local parser = vim.treesitter.get_parser(bufnr)
+  local root = parser:parse()[1]:root()
+  local lang = parser:lang()
+  local query = react_use_directive_query[lang]
+  if not query then
+    local ok, maybe_query = pcall(vim.treesitter.query.parse, lang, react_use_directive_query_str)
+    if not ok then
+      return
+    end
+    query = maybe_query
+    react_use_directive_query[lang] = query
+  end
+  ---@diagnostic disable-next-line: missing-parameter
+  for _, node in query:iter_captures(root, bufnr) do
+    local directive = vim.treesitter.get_node_text(node, bufnr)
+    get_react_use_directive_cache[bufnr] = {
+      directive = directive,
+      -- add random jitter to avoid cache stampede
+      timestamp = vim.uv.now() + math.random(0, 1000),
+    }
+    return directive
+  end
+end
+
 incline.setup {
   render = function(props)
     local bufname = a.nvim_buf_get_name(props.buf)
@@ -24,16 +71,33 @@ incline.setup {
     local fg = props.focused and extra_colors.fg or extra_colors.fg_nc
     local bg = buf_focused and extra_colors.bg or extra_colors.bg_nc
 
-    local fname = bufname == '' and '[No name]' or vim.fn.fnamemodify(bufname, ':t')
+    ---@diagnostic disable-next-line: redundant-parameter
+    local filetype = a.nvim_buf_get_option(props.buf, 'filetype')
 
-    local icon, icon_fg
-    if bufname ~= '' then
-      icon, icon_fg = devicons.get_icon_color(fname)
+    local fname, icon, icon_fg
+    if bufname == '' then
+      fname = '[No name]'
+    else
+      icon, icon_fg = devicons.get_icon_color(bufname)
+      local Path = require 'plenary.path'
+      local sep = Path.path.sep
+      local rel = vim.fn.fnamemodify(bufname, ':.')
+      local components = vim.split(rel, sep)
+      if #components > 2 then
+        local head = { unpack(components, 1, #components - 2) }
+        local lastTwo = { components[#components - 1], components[#components] }
+        local shortHead = Path.new(unpack(head)):shorten(1, {})
+        fname = {
+          { shortHead, sep, guifg = extra_colors.fg_nc },
+          { table.concat(lastTwo, sep), guifg = extra_colors.fg },
+        }
+      else
+        fname = rel
+      end
     end
+
     if not icon or icon == '' then
       local icon_name
-      ---@diagnostic disable-next-line: redundant-parameter
-      local filetype = a.nvim_buf_get_option(props.buf, 'filetype')
       if filetype ~= '' then
         icon_name = devicons.get_icon_name_by_filetype(filetype)
       end
@@ -47,17 +111,35 @@ incline.setup {
     icon = icon or ''
     icon_fg = props.focused and (icon_fg or extra_colors.fg) or extra_colors.fg_nc
 
+    local extra = {}
+
+    if filetype == 'javascriptreact' or filetype == 'typescriptreact' then
+      local client_or_server = get_react_use_directive(props.buf)
+      if client_or_server then
+        local use_directive_icon = ({
+          ['use server'] = ' 󰬀󰫲󰫿󱂌󰫲󰫿 ',
+          ['use client'] = ' 󰫰󱎦󱂈󰫲󰫻󰬁 ',
+        })[client_or_server]
+        table.insert(extra, { use_directive_icon, guifg = fg })
+      end
+    end
+
     return {
+      extra,
       {
-        ' ' .. icon .. ' ',
-        guifg = buf_focused and colors.licorice or colors.deep_velvet,
-        guibg = props.focused and icon_fg or (buf_focused and icon_fg or nil),
+        {
+          ' ',
+          icon,
+          ' ',
+          guifg = buf_focused and colors.licorice or colors.deep_velvet,
+          guibg = props.focused and icon_fg or (buf_focused and icon_fg or nil),
+        },
+        { has_error and '  ' or ' ', guifg = props.focused and colors.red or nil },
+        { fname, gui = modified and 'bold,italic' or nil },
+        { modified and ' * ' or ' ', guifg = extra_colors.fg },
+        guibg = bg,
+        guifg = fg,
       },
-      { has_error and '  ' or ' ', guifg = props.focused and colors.red or nil },
-      { fname, gui = modified and 'bold,italic' or nil },
-      { modified and ' * ' or ' ', guifg = extra_colors.fg },
-      guibg = bg,
-      guifg = fg,
     }
   end,
   window = {
@@ -72,6 +154,7 @@ incline.setup {
   },
   hide = {
     cursorline = 'focused_win',
-    cursor_overlap = 'focused_win',
   },
 }
+
+return M
