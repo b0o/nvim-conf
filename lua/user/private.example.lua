3b64ebd5b23744339b3913621dedd7659778a8ea
Maddison Hellstrom <maddy@na.ai>
2023-08-29 16:09:21 -0700

    add explanation to private.example.lua

--- lua/user/private.example.lua
+++ lua/user/private.example.lua
@@ -1,3 +1,6 @@
+-- Copy this file to lua/user/private.lua and edit it to add your private configuration.
+-- lua/user/private.lua is ignored by git.
+
 local M = {}
 
 M.openai_api_key = 'sk-xxxxxxxxxxxxxxxxxxxx'
