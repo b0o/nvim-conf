931ec23a36b97eafa21121b224a40e88cb800b36
Maddison Hellstrom <maddy@na.ai>
2023-10-04 19:09:49 -0700

    fix: lazy load vim-move on key mappings

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -265,6 +265,12 @@ local plugins = {
       vim.g.move_key_modifier = 'C'
       vim.g.move_key_modifier_visualmode = 'C'
     end,
+    keys = {
+      { '<C-h>', mode = { 'n', 'v' } },
+      { '<C-j>', mode = { 'n', 'v' } },
+      { '<C-k>', mode = { 'n', 'v' } },
+      { '<C-l>', mode = { 'n', 'v' } },
+    },
   },
   {
     'windwp/nvim-autopairs',
