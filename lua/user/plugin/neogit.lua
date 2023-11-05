07a214e0abc5bb323c9848f82a7fb00140fee46d
Maddison Hellstrom <maddy@na.ai>
2023-11-05 13:00:05 -0800

    fix: migrate neogit mappings to new popup table

--- lua/user/plugin/neogit.lua
+++ lua/user/plugin/neogit.lua
@@ -81,16 +81,16 @@ neogit.setup {
     section = { '', '' },
   },
   -- Each Integration is auto-detected through plugin presence, however, it can be disabled by setting to `false`
-  integrations = {
-    -- If enabled, use telescope for menu selection rather than vim.ui.select.
-    -- Allows multi-select and some things that vim.ui.select doesn't.
-    -- telescope = nil,
-    -- Neogit only provides inline diffs. If you want a more traditional way to look at diffs, you can use `diffview`.
-    -- The diffview integration enables the diff popup.
-    --
-    -- Requires you to have `sindrets/diffview.nvim` installed.
-    -- diffview = false,
-  },
+  -- integrations = {
+  -- If enabled, use telescope for menu selection rather than vim.ui.select.
+  -- Allows multi-select and some things that vim.ui.select doesn't.
+  -- telescope = nil,
+  -- Neogit only provides inline diffs. If you want a more traditional way to look at diffs, you can use `diffview`.
+  -- The diffview integration enables the diff popup.
+  --
+  -- Requires you to have `sindrets/diffview.nvim` installed.
+  -- diffview = false,
+  -- },
   -- sections = {
   --   -- Reverting/Cherry Picking
   --   sequencer = {
@@ -139,6 +139,10 @@ neogit.setup {
   --   },
   -- },
   mappings = {
+    popup = {
+      ['Z'] = false,
+      ['<M-s>'] = 'StashPopup',
+    },
     -- finder = {
     --   ['<cr>'] = 'Select',
     --   ['<c-c>'] = 'Close',
@@ -183,8 +187,6 @@ neogit.setup {
       -- ['c'] = 'CommitPopup',
       -- ['l'] = 'LogPopup',
       -- ['v'] = 'RevertPopup',
-      ['Z'] = false,
-      ['<M-s>'] = 'StashPopup',
       -- ['A'] = 'CherryPickPopup',
       -- ['b'] = 'BranchPopup',
       -- ['f'] = 'FetchPopup',
