adca0edf7838700af1bd49bb1d99388d22da2407
Maddison Hellstrom <maddy@na.ai>
2023-11-06 23:35:27 -0800

    chore: tweak oil.nvim config

--- lua/user/plugin/oil.lua
+++ lua/user/plugin/oil.lua
@@ -9,5 +9,17 @@ oil.setup {
     padding = 2,
     max_width = 100,
     max_height = 40,
+    override = function(conf)
+      return vim.tbl_deep_extend('force', conf, {
+        zindex = 80,
+      })
+    end,
+  },
+  skip_confirm_for_simple_edits = true,
+  keymaps = {
+    ['<M-u>'] = 'actions.parent',
+    ['<M-i>'] = 'actions.select',
+    ['<C-v>'] = 'actions.select_vsplit',
+    ['<C-x>'] = 'actions.select_split',
   },
 }
