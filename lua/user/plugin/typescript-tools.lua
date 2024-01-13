e4b3b229fa20f85dfc7ce5685e2ca25844959587
Maddison Hellstrom <maddy@na.ai>
2024-01-12 16:37:54 -0800

    chore(typescript-tools): remove workaround for duplicate on_attach calls

--- lua/user/plugin/typescript-tools.lua
+++ lua/user/plugin/typescript-tools.lua
@@ -1,18 +1,7 @@
----@type Map<integer, Map<integer, boolean>> client -> (bufnr -> attached)
-local attached = {}
-
 require('typescript-tools').setup {
   on_attach = function(client, bufnr)
-    ---TODO: https://github.com/pmizio/typescript-tools.nvim/issues/208
-    if attached[client.id] and attached[client.id][bufnr] then
-      return
-    end
-    if not attached[client.id] then
-      attached[client.id] = {}
-    end
     require('twoslash-queries').attach(client, bufnr)
     require('user.lsp').on_attach(client, bufnr)
-    attached[client.id][bufnr] = true
   end,
   settings = {
     separate_diagnostic_server = true,
