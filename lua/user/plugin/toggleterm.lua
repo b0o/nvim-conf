ea50073b2e4cbaedd70339751d9992e55266de65
Maddison Hellstrom <maddy@na.ai>
2024-01-15 20:08:02 -0800

    chore: update toggleterm config

--- lua/user/plugin/toggleterm.lua
+++ lua/user/plugin/toggleterm.lua
@@ -2,76 +2,27 @@
 local xk = require('user.mappings').xk
 
 require('toggleterm').setup {
-  -- size can be a number or function which is passed the current terminal
-  -- size = 20 | function(term)
-  --   if term.direction == "horizontal" then
-  --     return 15
-  --   elseif term.direction == "vertical" then
-  --     return vim.o.columns * 0.4
-  --   end
-  -- end,
   size = function(term)
     if term.direction == 'horizontal' then
       return math.max(15, math.min(50, math.floor(vim.o.lines * 0.33)))
     elseif term.direction == 'vertical' then
-      return math.max(40, math.min(120, math.floor(vim.o.columns * 0.2)))
+      return math.max(80, math.min(120, math.floor(vim.o.columns * 0.2)))
     end
   end,
   open_mapping = xk [[<C-S-/>]],
-  -- on_create = fun(t: Terminal), -- function to run when the terminal is first created
   on_open = function()
     vim.cmd 'startinsert!'
   end,
-  -- on_close = fun(t: Terminal), -- function to run when the terminal closes
-  -- on_stdout = fun(t: Terminal, job: number, data: string[], name: string) -- callback for processing output on stdout
-  -- on_stderr = fun(t: Terminal, job: number, data: string[], name: string) -- callback for processing output on stderr
-  -- on_exit = fun(t: Terminal, job: number, exit_code: number, name: string) -- function to run when terminal process exits
-  -- hide_numbers = true, -- hide the number column in toggleterm buffers
-  -- shade_filetypes = {},
-  -- autochdir = false, -- when neovim changes it current directory the terminal will change it's own when next it's opened
-  -- highlights = {
-  --   -- highlights which map to a highlight group name and a table of it's values
-  --   -- NOTE: this is only a subset of values, any group placed here will be set for the terminal window split
-  --   Normal = {
-  --     guibg = "<VALUE-HERE>",
-  --   },
-  --   NormalFloat = {
-  --     link = 'Normal'
-  --   },
-  --   FloatBorder = {
-  --     guifg = "<VALUE-HERE>",
-  --     guibg = "<VALUE-HERE>",
-  --   },
-  -- },
-  shade_terminals = false, -- NOTE: this option takes priority over highlights specified so if you specify Normal highlights you should set this to false
-  -- shading_factor = '<number>', -- the percentage by which to lighten terminal background, default: -30 (gets multiplied by -3 if background is light)
-  -- start_in_insert = true,
-  -- insert_mappings = true, -- whether or not the open mapping applies in insert mode
-  -- terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
-  -- persist_size = true,
-  -- persist_mode = true, -- if set to true (default) the previous terminal mode will be remembered
-  -- direction = 'vertical' | 'horizontal' | 'tab' | 'float',
-  -- close_on_exit = true, -- close the terminal window when the process exits
-  -- shell = 'tmux -L tmux-nvim -f $XDG_CONFIG_HOME/tmux/tmux-nvim.conf',
-  -- auto_scroll = true, -- automatically scroll to the bottom on terminal output
-  -- -- This field is only relevant if direction is set to 'float'
+  persist_size = false,
+  shade_terminals = false,
   float_opts = {
-    --   -- The border key is *almost* the same as 'nvim_open_win'
-    --   -- see :h nvim_open_win for details on borders however
-    --   -- the 'curved' border is a custom border type
-    --   -- not natively supported but implemented in this plugin.
-    --   border = 'single' | 'double' | 'shadow' | 'curved' | ... other options supported by win open
     border = 'curved',
-    --   -- like `size`, width and height can be a number or function which is passed the current terminal
-    --   width = <value>,
-    --   height = <value>,
-    --   winblend = 3,
+    width = function()
+      return math.max(40, math.min(200, math.floor(vim.o.columns * 0.55)))
+    end,
+    height = function()
+      return math.max(30, math.min(100, math.floor(vim.o.lines * 0.55)))
+    end,
     zindex = 200,
   },
-  -- winbar = {
-  --   enabled = false,
-  --   name_formatter = function(term) --  term: Terminal
-  --     return term.name
-  --   end
-  -- },
 }
