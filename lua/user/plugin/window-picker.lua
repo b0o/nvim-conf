be903e260c44ccb9756ed47345f13e9a4f12a08e
Maddison Hellstrom <maddy@na.ai>
2024-02-13 12:47:13 -0800

    feat: custom window-picker font

--- lua/user/plugin/window-picker.lua
+++ lua/user/plugin/window-picker.lua
@@ -2,8 +2,212 @@
 
 local colors = require 'user.colors'
 
+local font = {
+  ['a'] = [[
+,-.
+,-|
+`-^ ]],
+
+  ['b'] = [[
+.
+|-.
+| |
+^-' ]],
+
+  ['c'] = [[
+,-.
+|
+`-' ]],
+
+  ['d'] = [[
+  .
+,-|
+| |
+`-^ ]],
+
+  ['e'] = [[
+,-.
+|-'
+`-' ]],
+
+  ['f'] = [[
+,"'
+|-
+|
+'  ]],
+
+  ['g'] = [[
+,-.
+| |
+`-|
+. |
+`'` ]],
+
+  ['h'] = [[
+|
+|-.
+| |
+' ' ]],
+
+  ['i'] = [[
+'
+|
+| ]],
+
+  ['j'] = [[
+  '
+  |
+, |
+`'` ]],
+
+  ['k'] = [[
+.
+| ,
+|<
+' ` ]],
+
+  ['l'] = [[
+.
+|
+|
+'-]],
+
+  ['m'] = [[
+,-,-.
+| | |
+' ' ' ]],
+
+  ['n'] = [[
+,-.
+| |
+' ' ]],
+
+  ['o'] = [[
+,-.
+| |
+`-' ]],
+
+  ['p'] = [[
+,-.
+| |
+|-'
+|
+'   ]],
+
+  ['q'] = [[
+,-.
+| |
+`-|
+  |
+  `-]],
+
+  ['r'] = [[
+,-.
+|
+'   ]],
+
+  ['s'] = [[
+,-.
+`-.
+`-' ]],
+
+  ['t'] = [[
+ .
+-|-
+ |
+ `' ]],
+
+  ['u'] = [[
+. .
+| |
+`-^ ]],
+
+  ['v'] = [[
+.  ,
+| /
+`'   ]],
+
+  ['w'] = [[
+. , ,
+|/|/
+' '   ]],
+
+  ['x'] = [[
+\ /
+ X
+/ \ ]],
+
+  ['y'] = [[
+. .
+| |
+`-|
+ /|
+`-' ]],
+
+  ['z'] = [[
+,_,
+ /
+'"' ]],
+
+  ['0'] = [[
+,-.
+|/|
+`-' ]],
+
+  ['1'] = [[
+ ,
+'|
+ ` ]],
+
+  ['2'] = [[
+,-,
+ /
+'-` ]],
+
+  ['3'] = [[
+,-.
+ -<
+`-' ]],
+
+  ['4'] = [[
+ ,.
+{_|
+  ' ]],
+
+  ['5'] = [[
+.--
+`-.
+`-' ]],
+
+  ['6'] = [[
+,-.
+|-.
+`-' ]],
+
+  ['7'] = [[
+--,
+ /
+'   ]],
+
+  ['8'] = [[
+,-.
+>-<
+`-' ]],
+
+  ['9'] = [[
+,-.
+`-|
+`-' ]],
+
+  [';'] = [[
+:;
+:;
+,' ]],
+}
+
 require('window-picker').setup {
   hint = 'floating-big-letter',
+  selection_chars = 'FJDKSLACMRUEIWOQPHTGYVBNZX',
   filter_rules = {
     autoselect_one = false,
     -- filter using buffer options
@@ -12,10 +216,21 @@ require('window-picker').setup {
       filetype = { 'neo-tree', 'neo-tree-popup', 'notify', 'incline' },
 
       -- if the buffer type is one of following, the window will be ignored
-      buftype = { 'terminal', 'quickfix' },
+      buftype = { 'quickfix' },
+    },
+  },
+  picker_config = {
+    floating_big_letter = {
+      -- window picker plugin provides bunch of big letter fonts
+      -- fonts will be lazy loaded as they are being requested
+      -- additionally, user can pass in a table of fonts in to font
+      -- property to use instead
+      font = font,
     },
   },
   -- other_win_hl_color = colors.deep_anise,
   fg_color = colors.hydrangea,
   show_prompt = false,
 }
+
+return font
