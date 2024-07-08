29499b0acba2fee37b1740a6d6095aced4654642
Maddison Hellstrom <maddy@na.ai>
2024-07-07 19:12:01 -0700

    feat: add gen.nvim

--- lua/user/plugins/ai.lua
+++ lua/user/plugins/ai.lua
@@ -9,4 +9,17 @@ end)
 return {
   'zbirenbaum/copilot.lua',
   'supermaven-inc/supermaven-nvim',
+  {
+    'David-Kunz/gen.nvim',
+    cmd = { 'Gen' },
+    opts = {
+      -- model = 'llama3',
+      model = 'codegemma',
+      host = 'localhost',
+      port = '11434',
+      quit_map = 'q',
+      retry_map = '<C-r>',
+      show_model = true,
+    },
+  },
 }
