26e2a285691b546b6d19a97b0fd54518096097c7
Maddison Hellstrom <maddy@na.ai>
2024-07-07 19:15:16 -0700

    feat: add AutocmdEvent type definition

--- .types.lua
+++ .types.lua
@@ -4,3 +4,12 @@
 ---@operator call:any
 
 ---@alias Callable fun(...)|CallableTable
+
+---@class AutocmdEvent
+---@field id number @the id of the autocommand
+---@field event string @the name of the triggered event
+---@field group number|nil @the autocommand group id, if any
+---@field match string @the expanded value of <amatch>
+---@field buf number @the expanded value of <abuf>
+---@field file string @the expanded value of <afile>
+---@field data any @arbitrary data passed from `nvim_exec_autocmds()`
