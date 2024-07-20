aca4c7e0046ffa777c9cb32433c314efb21644fe
Maddison Hellstrom <maddy@na.ai>
2024-07-20 06:41:12 -0700

    feat: add gitlinker.nvim

--- lua/user/plugins/git.lua
+++ lua/user/plugins/git.lua
@@ -171,6 +171,37 @@ return {
     end,
   },
   {
+    'ruifm/gitlinker.nvim',
+    cmd = { 'GitLink' },
+    otps = {},
+    keys = {
+      {
+        '<leader>gb',
+        '<cmd>lua require"gitlinker".get_buf_range_url("n", {action_callback = require"gitlinker.actions".open_in_browser})<cr>',
+        mode = 'n',
+        desc = 'GitLinker: Open in browser',
+      },
+      {
+        '<leader>gb',
+        '<cmd>lua require"gitlinker".get_buf_range_url("v", {action_callback = require"gitlinker.actions".open_in_browser})<cr>',
+        mode = 'v',
+        desc = 'GitLinker: Open in browser',
+      },
+      {
+        '<leader>gy',
+        '<cmd>lua require"gitlinker".get_buf_range_url("n")<cr>',
+        mode = 'n',
+        desc = 'GitLinker: Copy URL',
+      },
+      {
+        '<leader>gy',
+        '<cmd>lua require"gitlinker".get_buf_range_url("v")<cr>',
+        mode = 'v',
+        desc = 'GitLinker: Copy URL',
+      },
+    },
+  },
+  {
     'NeogitOrg/neogit',
     cmd = 'Neogit',
     config = function()
