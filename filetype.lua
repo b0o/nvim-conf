10ab2bd79ed041dcca68ecfb919fe16071e3068e
Maddison Hellstrom <maddy@na.ai>
2024-12-02 15:52:43 -0800

    chore: simplify filype.lua

--- filetype.lua
+++ filetype.lua
@@ -2,16 +2,11 @@ vim.filetype.add {
   extension = {
     astro = 'astro',
     mdx = 'mdx',
+    pyx = 'cython',
+    pxd = 'cython',
   },
   pattern = {
     ['**/__snapshots__/*.ts.snap'] = { 'jsonc' },
     ['**/__snapshots__/*.js.snap'] = { 'jsonc' },
-  },
-}
-
-vim.filetype.add {
-  extension = {
-    pyx = 'cython',
-    pxd = 'cython',
   },
 }
