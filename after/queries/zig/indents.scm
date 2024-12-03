501da7b3ce892ed516b8b7a4b7181774b0911897
Maddison Hellstrom <maddy@na.ai>
2024-12-02 17:05:52 -0800

    fix(tree-sitter): indents for zig

---
+++ after/queries/zig/indents.scm
@@ -0,0 +1,8 @@
+; inherits: zig
+; TODO: Remove once merged:
+; https://github.com/nvim-treesitter/nvim-treesitter/pull/7199
+[
+  (opaque_declaration)
+  (enum_declaration)
+  (union_declaration)
+] @indent.begin
