2c177f6e1cd264f1d3d9fd1f1044a62dd946a161
Maddison Hellstrom <maddy@na.ai>
2024-12-12 22:03:07 -0800

    chore: lazy load vimtex

--- lua/user/plugins/langs.lua
+++ lua/user/plugins/langs.lua
@@ -273,8 +273,7 @@ local spec = {
   },
   {
     'lervag/vimtex',
-    lazy = false, -- we don't want to lazy load VimTeX
-    -- tag = "v2.15", -- uncomment to pin to a specific release
+    ft = { 'tex', 'bib' },
     init = function()
       vim.g.vimtex_imaps_enabled = 0
       vim.g.vimtex_view_method = 'zathura_simple'
