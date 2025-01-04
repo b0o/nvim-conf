3c5eb220a8d0439871b91ba6c5d077b0517a529a
Maddison Hellstrom <maddy@na.ai>
2025-01-03 17:49:11 -0800

    chore: attempt to make obsidian.nvim work with blink.cmp

--- lua/user/plugins/obsidian.lua
+++ lua/user/plugins/obsidian.lua
@@ -5,7 +5,6 @@ return {
   {
     'epwalsh/obsidian.nvim',
     dev = true,
-    -- version = '*',
     event = {
       ('BufReadPre %s/**.md'):format(private.obsidian_vault.path),
       ('BufNewFile %s/**.md'):format(private.obsidian_vault.path),
@@ -36,25 +35,33 @@ return {
       vim.o.conceallevel = 1
 
       require('obsidian').setup {
+        ui = { enable = false },
         workspaces = {
           private.obsidian_vault,
         },
-        completion = {
-          nvim_cmp = true,
-          min_chars = 1,
-        },
+        ---@diagnostic disable-next-line: missing-fields
+        completion = { nvim_cmp = false },
+        ---@diagnostic disable-next-line: missing-fields
         templates = {
           subdir = 'Meta/Templates',
           date_format = '%Y-%m-%d',
           time_format = '%H:%M',
           substitutions = {},
         },
+        ---@diagnostic disable-next-line: missing-fields
         daily_notes = {
           folder = 'Journal',
           date_format = '%Y/%Y-%m/%Y-%m-%d',
           template = 'JournalNvim.md',
         },
       }
+
+      -- HACK: fix error, disable completion.nvim_cmp option, manually register sources
+      -- See: https://github.com/epwalsh/obsidian.nvim/issues/770#issuecomment-2557300925
+      local cmp = require 'cmp'
+      cmp.register_source('obsidian', require('cmp_obsidian').new())
+      cmp.register_source('obsidian_new', require('cmp_obsidian_new').new())
+      cmp.register_source('obsidian_tags', require('cmp_obsidian_tags').new())
 
       vim.cmd.delcommand 'Rename'
       vim.cmd.cabbrev { 'Rename', 'ObsidianRename' }
@@ -64,7 +71,8 @@ return {
         local map = maputil.map
         local ft = maputil.ft
 
-        map('n', { '<C-f><C-f>', '<C-f>o', '<C-f><C-o>' }, '<Cmd>ObsidianTitles<Cr>', 'Obsidian: Search Titles')
+        map('n', '<C-f><C-f>', '<Cmd>ObsidianQuickSwitch<Cr>', 'Obsidian: Quick Switch')
+        map('n', { '<C-f>o', '<C-f><C-o>' }, '<Cmd>ObsidianTitles<Cr>', 'Obsidian: Search Titles')
 
         map('n', '<C-p>', function()
           vim.api.nvim_feedkeys(':Obsidian', 't', false)
