1547aeeafa3ace7af5001bcfd0fc24950237d20d
Maddison Hellstrom <maddy@na.ai>
2025-01-03 17:44:12 -0800

    chore: update blink.cmp config

--- lua/user/plugins/completion.lua
+++ lua/user/plugins/completion.lua
@@ -1,18 +1,58 @@
 local xk = require('user.keys').xk
 
+very_lazy(function()
+  local autocmd = vim.api.nvim_create_autocmd
+  local group = vim.api.nvim_create_augroup('user-cmp', { clear = true })
+  local orig_menu_border
+  autocmd('CmdlineEnter', {
+    group = group,
+    callback = function(ev)
+      --- HACK: Make the menu border blend with noice's cmdline border
+      local win = require('blink.cmp.completion.windows.menu').win
+      orig_menu_border = win.config.border
+      if ev.match == ':' then
+        win.config.border = {
+          '┐',
+          ' ',
+          '┌',
+          '│',
+          '╯',
+          '─',
+          '╰',
+          '│',
+        }
+      end
+    end,
+  })
+  autocmd('CmdlineLeave', {
+    group = group,
+    callback = function()
+      if orig_menu_border then
+        local win = require('blink.cmp.completion.windows.menu').win
+        win.config.border = orig_menu_border
+        orig_menu_border = nil
+      end
+    end,
+  })
+end)
+
 ---@type LazySpec[]
 return {
   {
     'saghen/blink.cmp',
+    dependencies = {
+      {
+        'saghen/blink.compat',
+        opts = {},
+      },
+    },
     lazy = false, -- lazy loading handled internally
     -- dev = true,
-    build = 'cargo build --release',
+    -- run with `zsh -i` so Mise loads proper Rust toolchain:
+    build = 'zsh -ic "cargo build --release"',
     opts = {
-      -- Available commands:
-      --   show, hide, cancel, accept, select_and_accept, select_prev, select_next,
-      --   show_documentation, hide_documentation, scroll_documentation_up,
-      --   scroll_documentation_down, snippet_forward, snippet_backward, fallback
       keymap = {
+        preset = 'default',
         [xk [[<C-S-a>]]] = { 'show', 'hide' },
         ['<CR>'] = { 'accept', 'fallback' },
         ['<Tab>'] = { 'snippet_forward', 'fallback' },
@@ -21,8 +61,61 @@ return {
         ['<Down>'] = { 'select_next', 'fallback' },
         ['<C-p>'] = { 'select_prev', 'show' },
         ['<C-n>'] = { 'select_next', 'show' },
+        [xk [[<C-S-n>]]] = { 'select_next', 'show' },
+        [xk [[<C-S-p>]]] = { 'select_prev', 'show' },
         ['<C-k>'] = { 'scroll_documentation_up', 'fallback' },
         ['<C-j>'] = { 'scroll_documentation_down', 'fallback' },
+        cmdline = {
+          ['<CR>'] = {
+            function()
+              vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<C-]><Cr>', true, true, true), 'n', true)
+              return true
+            end,
+          },
+          [xk [[<C-Cr>]]] = {
+            function(cmp)
+              return cmp.select_and_accept {
+                callback = function()
+                  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<CR>', true, true, true), 'n', true)
+                end,
+              }
+            end,
+            'fallback',
+          },
+          [xk [[<C-S-a>]]] = { 'show', 'hide' },
+          ['<Tab>'] = {
+            'show',
+            function()
+              local cmp = require 'blink.cmp'
+              if cmp.is_visible() then
+                if require('blink.cmp.completion.list').selected_item_idx == nil then
+                  return false
+                end
+                if cmp.accept() then
+                  vim.schedule(
+                    function()
+                      vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Space>', true, true, true), 'n', true)
+                    end
+                  )
+                  return true
+                end
+              end
+            end,
+            'select_next',
+            'fallback',
+          },
+          ['<S-Tab>'] = { 'show', 'select_prev', 'fallback' },
+          ['<C-p>'] = {
+            'select_prev',
+            'show',
+            'fallback',
+          },
+          ['<C-n>'] = {
+            'select_next',
+            'show',
+            'fallback',
+          },
+        },
       },
       completion = {
         list = {
@@ -44,6 +137,14 @@ return {
               },
             },
           },
+          cmdline_position = function()
+            if vim.g.ui_cmdline_pos ~= nil then
+              local pos = vim.g.ui_cmdline_pos -- (1, 0)-indexed
+              return { pos[1] - 1, pos[2] + 3 }
+            end
+            local height = (vim.o.cmdheight == 0) and 1 or vim.o.cmdheight
+            return { vim.o.lines - height, 0 }
+          end,
         },
         documentation = {
           auto_show = true,
@@ -60,166 +161,43 @@ return {
         window = {
           border = 'rounded',
           scrollbar = true,
-          direction_priority = { 's', 'n' },
+          direction_priority = { 'n', 's' },
         },
       },
       appearance = {
         nerd_font_variant = 'mono',
       },
-    },
-  },
-  {
-    -- TODO: Remove once blink.cmp supports command-line completion
-    -- https://github.com/Saghen/blink.cmp/pull/323
-    'hrsh7th/nvim-cmp',
-    config = function()
-      local cmp = require 'cmp'
-      local feedkeys = require 'cmp.utils.feedkeys'
-      local keymap = require 'cmp.utils.keymap'
-
-      local wincfg = vim.tbl_extend('force', cmp.config.window.bordered(), {
-        winhighlight = 'Normal:CmpNormal,FloatBorder:CmpBorder,CursorLine:CmpSel,Search:None',
-      })
-
-      local function wrap(fn, ...)
-        local args = { ... }
-        return function() return fn(unpack(args)) end
-      end
-
-      -- Select item next/prev, taking into account whether the cmp window is
-      -- top-down or bottoom-up so that the movement is always in the same direction.
-      local select_item_smart = function(dir, opts)
-        return function(fallback)
-          opts = opts or { behavior = cmp.SelectBehavior.Select }
-          fallback = opts.fallback or fallback
-          if cmp.visible() then
-            ---@diagnostic disable-next-line: invisible
-            if cmp.core.view.custom_entries_view:is_direction_top_down() then
-              ({ next = cmp.select_next_item, prev = cmp.select_prev_item })[dir](opts)
-            else
-              ({ prev = cmp.select_next_item, next = cmp.select_prev_item })[dir](opts)
-            end
-          elseif vim.bo.ft == 'dap-repl' then
-            feedkeys.call(keymap.t(({ next = '<Down>', prev = '<Up>' })[dir]), 'i', false)
-          else
-            fallback()
-          end
-        end
-      end
-
-      cmp.setup {
-        window = {
-          completion = wincfg,
-          documentation = wincfg,
+      sources = {
+        default = {
+          'lsp',
+          'path',
+          'snippets',
+          'buffer',
+          'lazydev',
+          'obsidian',
+          'obsidian_new',
+          'obsidian_tags',
         },
-        formatting = {
-          fields = { 'kind', 'abbr', 'menu' },
-          expandable_indicator = true,
-          format = function(entry, vim_item)
-            vim_item.menu = ({
-              cmdline = ' Cmd',
-            })[entry.source.name] or entry.source.name
-            local sym = require('lspkind').symbolic(vim_item.kind)
-            if sym == '' then
-              sym = '∅'
-            end
-            vim_item.menu = (vim_item.menu or '') .. '->' .. (vim_item.kind or '')
-            vim_item.kind = ' ' .. sym .. ' '
-            return vim_item
-          end,
-        },
-        sources = {},
-        enabled = function() return vim.bo.buftype ~= 'prompt' end,
-      }
-
-      cmp.setup.cmdline('/', {
-        mapping = cmp.mapping.preset.cmdline {
-          ['<C-z>'] = { c = false },
-          ['<C-n>'] = { c = select_item_smart('next', { fallback = wrap(feedkeys.call, keymap.t '<Down>', 'n') }) },
-          ['<C-p>'] = { c = select_item_smart('prev', { fallback = wrap(feedkeys.call, keymap.t '<Up>', 'n') }) },
-          [xk '<C-S-n>'] = {
-            c = function()
-              cmp.close()
-              feedkeys.call(keymap.t '<Down>', 'n')
-              cmp.complete()
-            end,
+        providers = {
+          lazydev = {
+            name = 'LazyDev',
+            module = 'lazydev.integrations.blink',
+            fallbacks = { 'lsp' },
           },
-          [xk '<C-S-p>'] = {
-            c = function()
-              cmp.close()
-              feedkeys.call(keymap.t '<Up>', 'n')
-              cmp.complete()
-            end,
+          obsidian = {
+            name = 'obsidian',
+            module = 'blink.compat.source',
+          },
+          obsidian_new = {
+            name = 'obsidian_new',
+            module = 'blink.compat.source',
+          },
+          obsidian_tags = {
+            name = 'obsidian_tags',
+            module = 'blink.compat.source',
           },
         },
-        sources = {
-          { name = 'buffer' },
-        },
-      })
-
-      local function handle_tab_complete(direction)
-        return function()
-          if vim.api.nvim_get_mode().mode == 'c' and cmp.get_selected_entry() == nil then
-            local text = vim.fn.getcmdline()
-            local expanded = vim.fn.expandcmd(text)
-            if expanded ~= text then
-              vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<C-U>', true, true, true) .. expanded, 'n', false)
-              cmp.complete()
-            elseif cmp.visible() then
-              direction()
-            else
-              cmp.complete()
-            end
-          else
-            if cmp.visible() then
-              direction()
-            else
-              cmp.complete()
-            end
-          end
-        end
-      end
-
-      cmp.setup.cmdline(':', {
-        mapping = cmp.mapping.preset.cmdline {
-          ['<Tab>'] = { c = handle_tab_complete(cmp.select_next_item) },
-          ['<S-Tab>'] = { c = handle_tab_complete(cmp.select_prev_item) },
-          ['<C-n>'] = { c = select_item_smart('next', { fallback = wrap(feedkeys.call, keymap.t '<Down>', 'n') }) },
-          ['<C-p>'] = { c = select_item_smart('prev', { fallback = wrap(feedkeys.call, keymap.t '<Up>', 'n') }) },
-          ['<C-e>'] = { c = cmp.mapping.close() },
-          [xk '<C-S-n>'] = {
-            c = function()
-              cmp.close()
-              feedkeys.call(keymap.t '<Down>', 'n')
-              cmp.complete()
-            end,
-          },
-          [xk '<C-S-p>'] = {
-            c = function()
-              cmp.close()
-              feedkeys.call(keymap.t '<Up>', 'n')
-              cmp.complete()
-            end,
-          },
-          [xk [[<C-S-a>]]] = {
-            c = function()
-              if cmp.visible() then
-                cmp.close()
-              else
-                cmp.complete()
-              end
-            end,
-          },
-        },
-        sources = cmp.config.sources {
-          { name = 'cmdline' },
-        },
-      })
-    end,
-    event = 'VeryLazy',
-    dependencies = {
-      'hrsh7th/cmp-cmdline',
-      'hrsh7th/cmp-buffer',
+      },
     },
   },
 }
